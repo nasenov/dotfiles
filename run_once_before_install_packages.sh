61abf42362362a78786362250356339097f507c7
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2025-08-13 22:55:20 +0300

    chore: disable arch install packages

--- run_once_before_install_packages.sh
+++ run_once_before_install_packages.sh
@@ -1,50 +1,52 @@
 #!/bin/sh
 
-sudo pacman -Syu --noconfirm
+# sudo pacman -Syu --noconfirm
 
 # CLI
-sudo pacman -S --needed --noconfirm man-db
-sudo pacman -S --needed --noconfirm base-devel
-sudo pacman -S --needed --noconfirm git
-sudo pacman -S --needed --noconfirm ttf-jetbrains-mono-nerd
-sudo pacman -S --needed --noconfirm hyprpolkitagent
-sudo pacman -S --needed --noconfirm hyprpaper
-sudo pacman -S --needed --noconfirm hyprpicker
-sudo pacman -S --needed --noconfirm hyprlock
-sudo pacman -S --needed --noconfirm hypridle
-sudo pacman -S --needed --noconfirm zsh
-sudo pacman -S --needed --noconfirm kubectl
-sudo pacman -S --needed --noconfirm krew
-sudo pacman -S --needed --noconfirm helm
-sudo pacman -S --needed --noconfirm fluxcd
-sudo pacman -S --needed --noconfirm talosctl
-sudo pacman -S --needed --noconfirm sops
-sudo pacman -S --needed --noconfirm terraform
-sudo pacman -S --needed --noconfirm ansible
-sudo pacman -S --needed --noconfirm yamllint
-sudo pacman -S --needed --noconfirm tflint
-sudo pacman -S --needed --noconfirm jdk21-openjdk
-sudo pacman -S --needed --noconfirm go-task
-sudo pacman -S --needed --noconfirm 7zip
-sudo pacman -S --needed --noconfirm unzip
-sudo pacman -S --needed --noconfirm openvpn
-sudo pacman -S --needed --noconfirm nfs-utils
+# sudo pacman -S --needed --noconfirm man-db
+# sudo pacman -S --needed --noconfirm base-devel
+# sudo pacman -S --needed --noconfirm git
+# sudo pacman -S --needed --noconfirm ttf-jetbrains-mono-nerd
+# sudo pacman -S --needed --noconfirm hyprpolkitagent
+# sudo pacman -S --needed --noconfirm hyprpaper
+# sudo pacman -S --needed --noconfirm hyprpicker
+# sudo pacman -S --needed --noconfirm hyprlock
+# sudo pacman -S --needed --noconfirm hypridle
+# sudo pacman -S --needed --noconfirm zsh
+# sudo pacman -S --needed --noconfirm kubectl
+# sudo pacman -S --needed --noconfirm krew
+# sudo pacman -S --needed --noconfirm helm
+# sudo pacman -S --needed --noconfirm fluxcd
+# sudo pacman -S --needed --noconfirm talosctl
+# sudo pacman -S --needed --noconfirm sops
+# sudo pacman -S --needed --noconfirm terraform
+# sudo pacman -S --needed --noconfirm ansible
+# sudo pacman -S --needed --noconfirm yamllint
+# sudo pacman -S --needed --noconfirm tflint
+# sudo pacman -S --needed --noconfirm jdk21-openjdk
+# sudo pacman -S --needed --noconfirm go-task
+# sudo pacman -S --needed --noconfirm 7zip
+# sudo pacman -S --needed --noconfirm unzip
+# sudo pacman -S --needed --noconfirm openvpn
+# sudo pacman -S --needed --noconfirm nfs-utils
 
 # GUI
-sudo pacman -S --needed --noconfirm obsidian
-sudo pacman -S --needed --noconfirm telegram-desktop
-sudo pacman -S --needed --noconfirm vlc
-sudo pacman -S --needed --noconfirm discord
-sudo pacman -S --needed --noconfirm code
-sudo pacman -S --needed --noconfirm dbeaver
-sudo pacman -S --needed --noconfirm libreoffice-fresh
-sudo pacman -S --needed --noconfirm freerdp
-sudo pacman -S --needed --noconfirm remmina
-yay -S --needed --noconfirm --removemake google-chrome
-yay -S --needed --noconfirm --removemake ledger-live-bin
-yay -S --needed --noconfirm --removemake intellij-idea-ultimate-edition
-yay -S --needed --noconfirm --removemake docker-desktop
+# sudo pacman -S --needed --noconfirm obsidian
+# sudo pacman -S --needed --noconfirm telegram-desktop
+# sudo pacman -S --needed --noconfirm vlc
+# sudo pacman -S --needed --noconfirm discord
+# sudo pacman -S --needed --noconfirm code
+# sudo pacman -S --needed --noconfirm dbeaver
+# sudo pacman -S --needed --noconfirm libreoffice-fresh
+# sudo pacman -S --needed --noconfirm freerdp
+# sudo pacman -S --needed --noconfirm remmina
+# yay -S --needed --noconfirm --removemake google-chrome
+# yay -S --needed --noconfirm --removemake ledger-live-bin
+# yay -S --needed --noconfirm --removemake intellij-idea-ultimate-edition
+# yay -S --needed --noconfirm --removemake docker-desktop
 
-kubectl krew install browse-pvc
+# kubectl krew install browse-pvc
 
-sudo ln -sf "$(which go-task)" /usr/local/bin/task
+# sudo ln -sf "$(which go-task)" /usr/local/bin/task
+
+# {{ if eq .chezmoi.os "linux" }}
