fdccdc707953f786da728ce11b00cb9c7406f022
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2025-08-15 15:13:46 +0300

    fix: add ledger udev rules

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -10,6 +10,8 @@
       ./hardware-configuration.nix
     ];
 
+  hardware.ledger.enable = true;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
