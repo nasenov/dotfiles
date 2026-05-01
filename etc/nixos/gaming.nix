8d7313c8c8cae96dbe872366d40fecc159e9f3e2
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-05-01 14:15:32 +0300

    feat: add desktop nixos configuration

---
+++ etc/nixos/gaming.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    discord
+    mangohud
+  ];
+
+  programs.steam.enable = true;
+
+  programs.gamemode = {
+    enable = true;
+    enableRenice = true;
+  };
+
+  users.users.nasenov.extraGroups = [ "gamemode" ];
+}
