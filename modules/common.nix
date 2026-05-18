2867e737c320d8a1f55014161faa10aadc96e915
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-05-18 22:34:45 +0300

    chore: add common nix module

---
+++ modules/common.nix
@@ -0,0 +1,111 @@
+{ pkgs, ... }:
+{
+  hardware.enableAllFirmware = true;
+
+  boot.plymouth.enable = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.editor = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  time.timeZone = "Europe/Sofia";
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  services.xserver.xkb = {
+    layout = "us,bg";
+    variant = ",phonetic";
+    options = "grp:alt_shift_toggle";
+  };
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  networking.networkmanager.enable = true;
+
+  users.defaultUserShell = pkgs.zsh;
+
+  users.users.nasenov = {
+    isNormalUser = true;
+    description = "Nikolay Asenov";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    bitwarden-cli
+    bws
+    chezmoi
+    dbeaver-bin
+    dnsutils
+    fluxcd
+    foot
+    foot.terminfo
+    foot.themes
+    git
+    go
+    go-task
+    google-chrome
+    helmfile
+    inetutils
+    jdk
+    jetbrains.idea
+    jq
+    krew
+    kubectl
+    kubernetes-helm
+    libreoffice
+    mise
+    nerd-fonts.jetbrains-mono
+    obsidian
+    oh-my-zsh
+    openssl
+    pciutils
+    rclone
+    talosctl
+    telegram-desktop
+    terraform
+    tflint
+    vim
+    vscode
+    yamllint
+    yq-go
+    zsh
+  ];
+
+  programs.foot.enable = true;
+
+  programs.zsh.enable = true;
+
+  system.autoUpgrade.enable = true;
+
+  system.stateVersion = "25.11";
+}
