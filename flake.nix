c63093ec139a6f9458f0dbc402d9727daabc64fb
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-06-17 19:42:09 +0300

    chore: use nixos-unstable for nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nasenov's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=master";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware?ref=master";
   };
 
