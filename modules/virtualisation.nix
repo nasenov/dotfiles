8ac26d057342430e5e962b7d2717a3dfb89067f0
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-08-06 19:49:52 +0300

    chore: remove qemu

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -5,18 +5,5 @@
     setSocketVariable = true;
   };
 
-  virtualisation.libvirtd = {
-    enable = true;
-
-    qemu = {
-      swtpm.enable = true;
-    };
-  };
-
-  programs.virt-manager.enable = true;
-
-  users.users.nasenov = {
-    linger = true;
-    extraGroups = [ "libvirtd" ];
-  };
+  users.users.nasenov.linger = true;
 }
